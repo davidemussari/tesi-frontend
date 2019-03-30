5e385d7fb9ad59ab7c80bf214a4c7466d8d574e3
davidemussari <mussaridavide@gmail.com>
2019-03-30 21:52:00 +0100

    tutorial - scomposizione con componente riutilizzabile

--- src/app/input-scomposizione/input-scomposizione.component.ts
+++ src/app/input-scomposizione/input-scomposizione.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-input-scomposizione',
-  templateUrl: './input-scomposizione.component.html',
-  styleUrls: ['./input-scomposizione.component.scss']
+selector: 'app-input-scomposizione',
+templateUrl: './input-scomposizione.component.html',
+styleUrls: ['./input-scomposizione.component.scss']
 })
 export class InputScomposizioneComponent implements OnInit {
+	
+	@Input() numeroScomposto;
+	@Input() indiceNumeroScomposto;
+	@Input() modaleScomposizione;
+	@Input() passaggi;
+	@Output() passaggiChange = new EventEmitter();
+	@Output() modaleScomposizioneChange = new EventEmitter();
+	
+	public decomposizione:string = '';
+	public decomposizioneCorretta: boolean = true; /*Variabile necessaria per avere alert di errore*/
+	
+	controlloDecomposizione(): void{
+	this.decomposizioneCorretta = this.decomposizione.match(/^[+-0123456789 ]+$/g) !== null && this.numeroScomposto == eval(this.decomposizione);
+	if(this.decomposizioneCorretta){
+		var temp : string[] = [];
+		var passaggio = this.decomposizione.replace(/\+/gi, ",+,").replace(/\-/gi, ",-,").split(',');
+		
+		for (var _i = 0; _i < this.passaggi[this.passaggi.length-1].length; _i++) {
+			if(_i == this.indiceNumeroScomposto){
+				for(let scomp of passaggio)
+					temp.push(scomp);
+			}else{
+				temp.push(this.passaggi[this.passaggi.length-1][_i]);
+			}
+		}
+		this.passaggi.push(temp);
+		this.passaggiChange.emit(this.passaggi);
+		this.modaleScomposizione = false; /*fa chiudere la modale*/
+		this.modaleScomposizioneChange.emit(this.modaleScomposizione);
+		this.decomposizione = '';
+	}
+}
 
-  constructor() { }
+constructor() { }
 
-  ngOnInit() {
-  }
+ngOnInit() {
+}
 
 }
--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { InputScomposizioneComponent } from '../input-scomposizione/input-scomposizione.component';
 
 @Component({
 selector: 'app-tutorial',
@@ -11,12 +12,20 @@ export class TutorialComponent implements OnInit {
 	public modaleScomposizione: boolean = false;
 	public numeroScomposto: number = 0;
 	public indiceNumeroScomposto: number = 0;
-	public decomposizione:string = '';
-	public decomposizioneCorretta: boolean = true; /*Variabile necessaria per avere alert di errore*/
 	public passaggi: Array<any> = [['12','+','17']];
+	@ViewChild(InputScomposizioneComponent) scomposizioneChild:InputScomposizioneComponent;
+	
+ngAfterViewInit() {
+	this.scomposizioneChild.controlloDecomposizione();
+}
+	
 constructor() {}
 
 ngOnInit() {
+}
+
+controlloDecomposizione(){
+	this.scomposizioneChild.controlloDecomposizione();
 }
 
 
@@ -30,25 +39,7 @@ indietro(ev)  :  void {
 		this.step += -1;
 }
 
-controlloDecomposizione(): void{
-	this.decomposizioneCorretta = this.decomposizione.match(/^[+-0123456789 ]+$/g) !== null && this.numeroScomposto == eval(this.decomposizione);
-	if(this.decomposizioneCorretta){
-		this.modaleScomposizione = false; /*chiude la modale*/
-		var temp : string[] = [];
-		var passaggio = this.decomposizione.replace(/\+/gi, ",+,").replace(/\-/gi, ",-,").split(',');
-		
-		for (var _i = 0; _i < this.passaggi[this.passaggi.length-1].length; _i++) {
-			if(_i == this.indiceNumeroScomposto){
-				for(let scomp of passaggio)
-					temp.push(scomp);
-			}else{
-				temp.push(this.passaggi[this.passaggi.length-1][_i]);
-			}
-		}
-		this.passaggi.push(temp);
-		this.decomposizione = '';
-	}
-}
+
 
 cliccabile(str: string): boolean{
 	return !isNaN(parseInt(str));
