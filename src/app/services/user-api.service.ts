9868f886cea64486d392129d7cfdb2c23b5ed4ac
Davide <mussaridavide@gmail.com>
2019-11-05 00:05:29 +0100

    Modifica ip pc per condivisione su rete lan

--- src/app/services/user-api.service.ts
+++ src/app/services/user-api.service.ts
@@ -7,8 +7,8 @@ import { User } from '../models/User';
 @Injectable()
 export class UserApiService {
 
-    //public static ulrServerBackEnd: String = 'http://192.168.1.103:8080/';
-    public static ulrServerBackEnd: String = 'http://localhost:8080/';
+    public static ulrServerBackEnd: String = 'http://192.168.1.103:8080/';
+    //public static ulrServerBackEnd: String = 'http://localhost:8080/';
 
     constructor(private http: HttpClient) { }
 
