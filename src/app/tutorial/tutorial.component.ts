22364fa4b920fdea85973868f8a2d5ef60dad51a
Davide <mussaridavide@gmail.com>
2019-11-12 16:26:13 +0100

    Pulizia codice

--- src/app/tutorial/tutorial.component.ts
+++ src/app/tutorial/tutorial.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
 import { InputScomposizioneComponent } from '../input-scomposizione/input-scomposizione.component';
 import { InputSommaAlgebricaComponent } from '../input-somma-algebrica/input-somma-algebrica.component';
 import { ImpostazioniGlobaliService } from '../services/impostazioni-globali.service';
@@ -11,7 +10,6 @@ import { ImpostazioniGlobaliService } from '../services/impostazioni-globali.ser
 })
 export class TutorialComponent implements OnInit {
 
-	public step: number = 0;
 	public paginaScomposizione: boolean = false;
 	public paginaAssociativa: boolean = false;
 	public numeroScomposto: number = 0;
@@ -32,15 +30,9 @@ export class TutorialComponent implements OnInit {
 	public valoreSpostato: string = '';
 	public eventoDrop: any;
 
-	ngAfterViewInit() {
-	}
-
 	constructor(private _impostazioniGlobali: ImpostazioniGlobaliService) {	}
 	
-	submit(){}
-
 	ngOnInit() {
-
 		// Questa e' l'iscrizione all'evento scatenato dal servizio
 		this._impostazioniGlobali.visualizzaPassaggiChange.subscribe(() => {
 			this.visualizzaPassaggi = this._impostazioniGlobali.visualizzaPassaggi;
