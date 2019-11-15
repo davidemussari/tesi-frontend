1de5bce7a08116ff8252c3c846b2faa49be6b155
Davide <mussaridavide@gmail.com>
2019-11-15 23:33:23 +0100

    Modifiche per ricevere set di soluzioni approvate con esercizio

--- src/app/models/Esercizio.ts
+++ src/app/models/Esercizio.ts
@@ -3,10 +3,12 @@ export class Esercizio {
 	public id: number;
     public tipologia: number;
     public testoEsercizio: any;
+	public soluzioni: any;
 
     constructor() {
         this.id = null;
         this.testoEsercizio = [];
         this.tipologia = null;
+        this.soluzioni = [];
     }
 }
